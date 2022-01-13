a6849a560fdf3a1807c3b6532fb5927bf766be65
Gregory Way <gregory.way@gmail.com>
2022-01-13 09:35:43 -0700

    add pearson facet, red highlight, change x axis label, and change file name

--- figures/scripts/nbconverted/LSA_results_perMOA.r
+++ figures/scripts/nbconverted/LSA_results_perMOA.r
@@ -15,7 +15,7 @@ plot_theme <- theme(
 )
 
 # Load L2 distances per MOA
-cp_file <- file.path("data", "MOA_LSA_zscores.tsv")
+cp_file <- file.path("data", "MOA_LSA_metrics.tsv")
 cp_df <- readr::read_tsv(cp_file)
 
 cp_df$model <- factor(
@@ -27,34 +27,60 @@ head(cp_df)
 
 # Compare performance means across architectures
 cp_mean_df <- cp_df %>%
-    dplyr::group_by(model) %>%
+    dplyr::group_by(model, metric) %>%
     dplyr::mutate(
         model_mean = round(mean(zscore), 2)
     ) %>%
-    dplyr::select(model, model_mean) %>%
+    dplyr::select(model, metric, model_mean) %>%
     dplyr::distinct()
 
 cp_mean_df
 
 plot_gg <- (
     ggplot(cp_df, aes(x=zscore))
-    + geom_histogram(aes(y = ..density..), bins=20)
+    + geom_histogram(aes(y = ..density..), bins = 30)
     + geom_density()
-    + geom_text(data=cp_mean_df, aes(x = -2, y = 0.35, label = paste0("Mean: ", model_mean)))
-    + geom_vline(data=cp_mean_df, aes(xintercept = model_mean), color = "red", linetype="dashed")
-    + facet_wrap("model", nrow = 5)
-    + geom_vline(xintercept = 0, linetype = "dashed", color = "blue")
+    + geom_text(
+        data=cp_mean_df,
+        aes(x = -3, y = 0.5, label = paste0("Mean: ", model_mean))
+    )
+    + geom_vline(
+        data=cp_mean_df,
+        aes(xintercept = model_mean),
+        color = "red",
+        linetype="dashed"
+    )
+    + geom_vline(
+        xintercept = 0,
+        linetype = "dashed",
+        color = "blue"
+    )
+    + facet_grid("model~metric")
     + theme_bw()
+    + xlim(c(-5, 5))
     + plot_theme
+    + geom_rect(
+        data = cp_df %>%
+            dplyr::filter(model == "MMD"),
+        fill = NA,
+        alpha = 1,
+        color = "red",
+        linetype = "solid",
+        size = 2,
+        xmin = -Inf,
+        xmax = Inf,
+        ymin = -Inf,
+        ymax = Inf
+    )
     + ylab("Density")
-    + xlab("Z-score of L2 distances between real and simulations\nfor predicted polypharmacology state compared to 10 random permutations")
+    + xlab("Z-score of metrics (L2 distance or Pearson correlation) for LSA predictions of\nground truth (per polypharmacology state) compared to 10 randomly shuffled LSA permutations")
     
 )
 plot_gg
 
 # Save figure
-output_file_base <- file.path("output", "sup_fig_lsa_zscores")
+output_file_base <- file.path("output", "sup_fig_lsa_metrics")
 for (file_extension in file_extensions) {
     output_file <- paste0(output_file_base, file_extension)
-    ggplot2::ggsave(output_file, plot_gg, dpi = 500, width = 6, height = 8)
+    ggplot2::ggsave(output_file, plot_gg, dpi = 500, width = 8, height = 8)
 }
